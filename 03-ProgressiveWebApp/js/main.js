68ff571020a66aa38e04a36fe319ee0c03342af1
Pawel Czachura <trybo1234@gmail.com>
2020-03-27 21:06:46 +0100

    Fixed pwa

--- 03-ProgressiveWebApp/js/main.js
+++ 03-ProgressiveWebApp/js/main.js
@@ -1,8 +1,8 @@
-window.onload = () => {
-    'use strict';
-
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('./sw.js');
-    }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
 }
--- 03-ProgressiveWebApp/sw.js
+++ 03-ProgressiveWebApp/sw.js
@@ -1,26 +1,24 @@
-var cacheName = 'bicycle';
-var filesToCache = [
-    '/',
-    '/index.html',
-    '/js/main.js',
-    "/images/icon1.png",
-    "/images/icon2.png"
+const staticDevCoffee = "dev-coffee-site-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/js/main.js",
+  "/images/icon1.png",
+  "/images/icon2.png"
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets);
+    })
+  );
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
 });
